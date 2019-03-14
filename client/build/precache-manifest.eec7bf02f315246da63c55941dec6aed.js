65695a17b4d2647dd6ea024bffd16142b7e380cd
cm718 <cm718@icloud.com>
2019-03-13 19:59:04 -0500

    build

---
+++ client/build/precache-manifest.eec7bf02f315246da63c55941dec6aed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "683291f979bb2e95116c",
+    "url": "/static/js/main.553a217b.chunk.js"
+  },
+  {
+    "revision": "4f1fe6162a83dee29630",
+    "url": "/static/js/2.d0405335.chunk.js"
+  },
+  {
+    "revision": "683291f979bb2e95116c",
+    "url": "/static/css/main.69ed4415.chunk.css"
+  },
+  {
+    "revision": "41ed3271825af292ba03769fac114174",
+    "url": "/index.html"
+  }
+];
